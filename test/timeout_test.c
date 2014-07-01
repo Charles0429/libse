e0743ee3483dfc8b54651ba24fb76fb1000b5668
Charles <xiezhenjiang@gmail.com>
2014-07-01 21:50:21 +0800

    fix code format

--- test/timeout_test.c
+++ test/timeout_test.c
@@ -13,18 +13,18 @@ struct event_loop *loop;
 
 int main(void)
 {
-	loop = event_loop_create();
+    loop = event_loop_create();
     event *e1, *e2;
-	int ret2 = event_loop_init(loop);
-	printf("%d", ret2);
-	printf("%s\n", loop->api->name);
-	e1 = event_create();
+    int ret2 = event_loop_init(loop);
+    printf("%d", ret2);
+    printf("%s\n", loop->api->name);
+    e1 = event_create();
     e2 = event_create();
-	event_set_timeout(e1, timeout1, (void *)e1, 1000);
+    event_set_timeout(e1, timeout1, (void *)e1, 1000);
     event_set_timeout(e2, timeout2, (void *)e2, 500);
-	event_register(loop, e1);
+    event_register(loop, e1);
     event_register(loop, e2);
-	event_loop_main(loop);	
+    event_loop_main(loop);	
 }
 
 void timeout1(int fd, short type, void *args)
